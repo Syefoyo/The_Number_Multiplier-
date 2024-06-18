ab8d4020f041ff51eec1da7edea2babd611f9de6
Syefoyo <trisyefolionel@gmail.com>
2024-06-18 19:24:53 +0700

    The Helper Of Multiplier

---
+++ script.js
@@ -0,0 +1,9 @@
+function times() {
+  let myInput = document.getElementById("kilometer").value;
+let myInputt = document.getElementById("miles").value;
+
+  let myText = document.getElementById("text");
+
+myText.innerHTML = myInput *
+  myInputt;
+}
